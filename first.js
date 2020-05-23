d7d5361472646651f3fc07da8c6311eda2bf649a
Chang sung beom <sbsk8185597@gmail.com>
2020-05-23 15:38:59 +0900

    Web front click listener

--- first.js
+++ first.js
@@ -1,6 +1,3 @@
-var body=document.body;
-var word=document.createElement('div');
-
-var word='장성범';
-
-document.body.append('장성범');
+document.getElementById('buttonTest').onclick=function(){
+    alert("asdfsf");
+};
